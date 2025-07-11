f26fc694e20e2016d883eff0e38e84946729a26a
Fahid Nasir <fahidnasir@hotmail.com>
2025-07-11 22:14:57 +0200

    Initial Commit

---
+++ ToggleWifi/ToggleWifiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToggleWifiApp.swift
+//  ToggleWifi
+//
+//  Created by Fahid Nasir on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ToggleWifiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
